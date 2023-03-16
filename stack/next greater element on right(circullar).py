18ccbcbca2c992cb76f6ea09b3476e634e21ee49
Drashtiie <112652566+Drashtiie@users.noreply.github.com>
2023-03-16 15:52:26 -0700

    Create next greater element on right(circullar).py

---
+++ stack/next greater element on right(circullar).py
@@ -0,0 +1,14 @@
+l= nums+nums
+
+        ans=[]
+        s=[]
+        for i in range(len(l)-1,-1,-1):
+            while len(s)>0 and s[-1] <= l[i]:
+                s.pop()
+            if len(s)==0:
+                ans.append(-1)
+            else:
+                ans.append(s[-1])
+            s.append(l[i])
+        ans.reverse()
+        return ans[:len(nums)]
