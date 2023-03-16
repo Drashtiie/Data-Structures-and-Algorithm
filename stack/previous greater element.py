3651875acd62d6fa30b256b5dd8621da80fe0c76
Drashtiie <112652566+Drashtiie@users.noreply.github.com>
2023-03-16 16:01:46 -0700

    Create previous greater element.py

---
+++ stack/previous greater element.py
@@ -0,0 +1,18 @@
+def nge(l):
+        nums= l
+        
+        
+        ans=[]
+        s=[]
+        for i in range(len(l)):
+            while len(s)>0 and s[-1] <= l[i]:
+                s.pop()
+            if len(s)==0:
+                ans.append(-1)
+            else:
+                ans.append(s[-1])
+            s.append(l[i])
+      
+        return ans[:len(nums)]
+        
+print(nge([7,1,2,3,4,5,2]))
